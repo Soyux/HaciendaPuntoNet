900c6b56e98dc14c2af689efe2a7feed7ed36182
Nestor Mora <nmorabonilla@gmail.com>
2021-11-27 00:40:00 -0800

    Skeleton of project

---
+++ HaciendaPuntoNET/HaciendaPuntoNET/Contracts/ITiqueteElectronico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaciendaPuntoNET.Contracts
+{
+    public interface ITiqueteElectronico
+    {
+
+
+    }//end of interface
+
+}//end of namespace
